811874e4b322d1225339d975e0e52d9ebcaeb78c
Nguyen Nhu Ngoc <nguyennhungoc2674@gmail.com>
2026-06-14 14:56:33 +0700

    NgocNN-ViewItemListPage

--- src/pages/players/viewPlayerList.jsx
+++ src/pages/players/viewPlayerList.jsx
@@ -10,11 +10,11 @@ import {
 } from "lucide-react";
 import { useEffect, useRef, useState, useCallback } from "react";
 import playerApi from "../../api/playerApi";
-import { PlayerDetailModal } from "./viewPlayerDetails.jsx";
-import { Button } from "../../components/common/button.jsx";
-import { SearchFilter } from "../../components/common/search_filtter.jsx";
-import { Pagination } from "../../components/common/pagination.jsx";
-import { Table, TableEmpty } from "../../components/common/table.jsx";
+import { PlayerDetailModal } from "./ViewPlayerDetails.jsx";
+import { Button } from "../../components/common/Button";
+import { SearchFilter } from "../../components/common/SearchFilter.jsx";
+import { Pagination } from "../../components/common/Pagination.jsx";
+import { Table, TableEmpty } from "../../components/common/Table.jsx";
 import "./css/user-actions.css";
 import "./css/user-status.css";
 
