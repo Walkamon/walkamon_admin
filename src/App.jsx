f0b0f2216d2fc1d27f34ff26d700152033834d87
Hung <hungtran010104@gmail.com>
2026-06-23 10:16:25 +0700

    Revert "fix import issue"
    This reverts commit 5d8ce4fd84bacf87bf7c5781664e13340ad34a61.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import {
 import { MainLayout } from "./components/layout/MainLayout";
 import { DashboardPage } from "./pages/dashboard/page/DashboardPage";
 import { UsersManagement } from "./pages/players/page/ViewPlayerList.jsx";
-import { authRoutes } from "./routes/authRoutes";
+import { authRoutes } from "../routes/AuthRoutes";
 import { ProtectedRoute } from "./routes/ProtectedRoute.jsx";
 import { ItemManagerPage } from "./pages/items/page/ItemManagerPage.jsx";
 import { ItemTypeManager } from "./pages/itemType/page/ItemTypeManager.jsx";
-import ReportListPage from "./pages/reports/viewReportList.jsx";
+import ReportListPage from "../pages/reports/ViewReportList.jsx";
 import ChallengesManagementPage from "./pages/challenges/page/ChallengesManagementPage.jsx";
 import MissionsManagement from "./pages/missions/MissionsManagement.jsx";
 import { ShopPage } from "./pages/shop/page/ShopManagerPage.jsx";
@@ -59,13 +59,13 @@ const router = createBrowserRouter([
             path: "missions",
             element: <MissionsManagement />,
           },
-          {
+           {
             path: "shop",
             element: <ShopPage />,
           },
         ],
       },
-    ],
+    ],  
   },
 
   {
